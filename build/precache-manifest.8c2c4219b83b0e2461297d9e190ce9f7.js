d10d65c47dad1b03973059cc55dbcd461411e6a8
NushySC <ank.sampedro@gmail.com>
2019-05-05 22:45:45 +0200

    "build"

---
+++ build/precache-manifest.8c2c4219b83b0e2461297d9e190ce9f7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8cb20c030c0c1f0d9214f470d5666a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "549c1f7439c715b18e1a",
+    "url": "/static/css/main.c0ada6bd.chunk.css"
+  },
+  {
+    "revision": "f53096bc8ebcfd083bb7",
+    "url": "/static/js/2.34ac0d43.chunk.js"
+  },
+  {
+    "revision": "549c1f7439c715b18e1a",
+    "url": "/static/js/main.11f8cc93.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
