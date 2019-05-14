ef01e01efd0e1e8fb881efe0cce22b9d8ee6c77c
Anuska <ank.sampedro@gmail.com>
2019-05-15 00:09:22 +0200

    some css

--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -6,43 +6,76 @@ let options = { weekday: 'short', month: 'short', day: 'numeric' };
 
 const Weather = (props) => {
 
-    let tempC = props.temperature
-    let tempF =  props.temperature * 9 / 5 + 32
-
     return (
 
         <div className={Styles.outputs}>
             
 
-            {props.id&& props.country && <div className={Styles.fontIcon}><i className={`owf owf-${props.id}`}></i></div>}
+            {props.id&& props.country && 
+                <div className={Styles.fontIcon}>
+                <i className={`owf owf-${props.id}`}></i>
+                </div>}
 
-                {props.city && props.country && <h2 className={Styles.outputCity}>{props.city}, {props.country}</h2>}
-                {props.main && props.description && <div className={Styles.outputDesc}>{props.main}/{props.description}</div>}
+            {props.city && props.country && 
+                <h2 className={Styles.outputCity}>{props.city}, {props.country}
+                </h2>}
+            {props.main && props.description && 
+                <div className={Styles.outputDesc}>{props.main}/{props.description}
+                </div>}
                 
 
-{props.id&& props.country && 
-    <>
-        <p className={Styles.toogleDegrees}>C/F</p>
-                    <Toggle />
-    </>}
+            <div className={Styles.outputDatas}>
+                <div className={Styles.outputData}>
+                    {props.temperature && 
+                        <div className={Styles.outputTitle}>Temperature
+                            <p className={Styles.outputResult}>{props.temperature}°C</p> 
+                        </div>}
+                </div>  
 
-
-                <div className={Styles.outputDatas}>
-                    <div className={Styles.outputData}>{props.temperature && <div className={Styles.outputTitle}>Temperature<p className={Styles.outputResult}>{props.temperature}°C</p> </div>}</div>  
-
-                    <div className={Styles.outputData}>{props.wind && <div className={Styles.outputTitle}>Wind<p className={Styles.outputResult}>{props.wind} m/s</p></div>}</div>
-
-                    <div className={Styles.outputData}>{props.humidity && <div className={Styles.outputTitle}>Humidity<p className={Styles.outputResult}>{props.humidity}%</p></div>}</div>
-                    
+                <div className={Styles.outputData}>
+                    {props.wind && 
+                        <div className={Styles.outputTitle}>Wind
+                            <p className={Styles.outputResult}>{props.wind} m/s</p>
+                        </div>}
                 </div>
 
-                {props.forecast1 && <div className={Styles.forecasts}>
+                <div className={Styles.outputData}>
+                    {props.humidity && 
+                        <div className={Styles.outputTitle}>Humidity
+                            <p className={Styles.outputResult}>{props.humidity}%</p>
+                        </div>}
+                </div>            
+            </div>
+
+                {props.forecast1 && 
+                    <div className={Styles.forecasts}>
                 
-                    <div className={Styles.forecast}>{props.forecast1 && <div className={Styles.nextTemp}><p><i className={`owf owf-${props.tomorrowIcon}`}></i></p><p className={Styles.future}>{props.tomorrow.toLocaleString('en-US', options)}</p><p className={Styles.future}>{props.forecast1} °C</p></div>}</div>
+                    <div className={Styles.forecast}>
+                        {props.forecast1 && 
+                            <div className={Styles.nextTemp}>
+                                <i className={`owf owf-${props.tomorrowIcon}`}></i>
+                                <p className={Styles.future}>{props.tomorrow.toLocaleString('en-US', options)}</p>
+                                <p className={Styles.future}>{props.forecast1} °C</p>
+                            </div>}
+                    </div>
 
-                    <div className={Styles.forecast}>{props.forecast2 && <div className={Styles.nextTemp}><i className={`owf owf-${props.afterTomorrowIcon}`}></i><p className={Styles.future}>{props.afterTomorrow.toLocaleString('en-US', options)}</p><p className={Styles.future}>{props.forecast2} °C</p></div>}</div>
+                    <div className={Styles.forecast}>
+                        {props.forecast2 && 
+                            <div className={Styles.nextTemp}>
+                                <i className={`owf owf-${props.afterTomorrowIcon}`}></i>
+                                <p className={Styles.future}>{props.afterTomorrow.toLocaleString('en-US', options)}</p>
+                                <p className={Styles.future}>{props.forecast2} °C</p>
+                            </div>}
+                    </div>
 
-                    <div className={Styles.forecast}>{props.forecast3 && <div className={Styles.nextTemp}><i className={`owf owf-${props.afterAfterTomorrowIcon}`}></i><p className={Styles.future}>{props.afterAfterTomorrow.toLocaleString('en-US', options)}</p><p className={Styles.future}>{props.forecast3} °C</p></div>}</div>
+                    <div className={Styles.forecast}>
+                        {props.forecast3 && 
+                            <div className={Styles.nextTemp}>
+                                <i className={`owf owf-${props.afterAfterTomorrowIcon}`}></i>
+                                <p className={Styles.future}>{props.afterAfterTomorrow.toLocaleString('en-US', options)}</p>
+                                <p className={Styles.future}>{props.forecast3} °C</p>
+                            </div>}
+                    </div>
                 
                 </div>}
                 
